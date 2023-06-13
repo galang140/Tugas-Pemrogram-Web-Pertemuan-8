d2836d286bc81ffcf8174278b842284bf1fcc355
galang <101632260+galang140@users.noreply.github.com>
2023-06-13 23:05:50 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import Navbar from '../src/components/Navbar.vue'
+  import Button from '../src/components/Hero.vue'
+import Hero from './components/Hero.vue';
+</script>
+
+<template>
+  <Navbar/>
+  <Hero/>
+</template>
